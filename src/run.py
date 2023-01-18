9e81aa8ea0c4021901a3edf983a41660542f9345
masoud mahdavi <masoudmahdavi1993@gmail.com>
2023-01-18 10:28:29 +0330

    make themes, profile.py, upadte run.py

--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from pathlib import Path
 
 st.title(":zap: Github Profile Readme")
 
@@ -19,4 +20,22 @@ with st.expander("Personal Info"):
 st.header("Social Media")
 with st.expander("Social Media"):
     col1, col2 = st.columns(2)
-    
+    github = col1.text_input("Github")
+    linkedin = col2.text_input("Linkdin")
+    twitter = col1.text_input("Twitter")
+    facebook = col2.text_input("Facebook")
+    instagram = col1.text_input("Instagram")
+    youtube = col2.text_input("Youtube")
+    medium = col1.text_input("Medium")
+
+
+# Select Theme
+st.header("Theme")
+themes = Path("src/themes").iterdir()
+themes = [theme.name for theme in themes]
+theme =st.selectbox("Select Theme", themes)
+st.markdown(f"Selected Theme: {theme}")
+
+
+# Generate Readme
+st.header("Generate Readme")
